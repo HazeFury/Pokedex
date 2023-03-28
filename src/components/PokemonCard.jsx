871acae088446ec7c5e6189d10becd4968e9f1aa
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2023-03-28 19:38:17 +0200

    Finished the first props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,34 +1,15 @@
 import "../PokemonCard.css"
 
 
- const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-
-
-function PokemonCard() {
-  
-  const pokemon = pokemonList[0];
+function PokemonCard({pokemon}) {
 
   return (
-
     <figure>
       {pokemon.imgSrc ? <img src= {pokemon.imgSrc} alt={pokemon.name}/>  :  <p>???</p>}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
-    
-  
-
-  )
-  }
+  );
+  };
   
 
  
